8e110d229d01ceb41b49dea669ec40742b2e6b40
Pawel <wilkowski@gmail.com>
2021-10-09 00:44:04 +0200

    update script

---
+++ update.js
@@ -0,0 +1,24 @@
+﻿const fs = require('fs');
+
+const v = process.argv[2];
+
+let prop = fs.readFileSync('./Properties/AssemblyInfo.cs').toString();
+
+prop = prop
+	.replace(/AssemblyFileVersion\("([^"\*]+)"\)]/g, `AssemblyFileVersion("${v}")`)
+	.replace(/AssemblyVersion\("([^"\*]+)"\)]/g, `AssemblyVersion("${v}")`);
+
+fs.writeFileSync('./Properties/AssemblyInfo.cs', prop);
+
+let manifest = fs.readFileSync('./NextUIPlugin/NextUIPlugin.json').toString();
+const dec = JSON.parse(manifest);
+dec.AssemblyVersion = v;
+
+fs.writeFileSync('./NextUIPlugin/NextUIPlugin.json', JSON.stringify(dec, null, 4));
+
+const execSync = require('child_process').execSync;
+
+console.log(execSync(`git add Properties/AssemblyInfo.cs`, { encoding: 'utf-8' }));
+console.log(execSync(`git add NextUIPlugin/NextUIPlugin.json`, { encoding: 'utf-8' }));
+console.log(execSync(`git commit -m v${v}`, { encoding: 'utf-8' }));
+console.log(execSync(`git tag -a v${v} -m v${v}`, { encoding: 'utf-8' }));
