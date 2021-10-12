961eaeacb7c78635bae8877c2062cf0b91783279
Pawel <wilkowski@gmail.com>
2021-10-13 01:32:50 +0200

    clean up

--- NextUIPlugin/Overlay/WndProcHandler.cs
+++ NextUIPlugin/Overlay/WndProcHandler.cs
@@ -2,42 +2,44 @@
 using System.Runtime.InteropServices;
 
 namespace NextUIPlugin.Overlay {
-	public class WndProcHandler {
+	public static class WndProcHandler {
 		public delegate (bool, long) WndProcMessageDelegate(WindowsMessage msg, ulong wParam, long lParam);
 
-		public static event WndProcMessageDelegate WndProcMessage;
+		public static event WndProcMessageDelegate? WndProcMessage;
 
 		public delegate long WndProcDelegate(IntPtr hWnd, uint msg, ulong wParam, long lParam);
 
-		protected static WndProcDelegate wndProcDelegate;
+		static WndProcDelegate? wndProcDelegate;
 
-		protected static IntPtr hWnd;
-		protected static IntPtr oldWndProcPtr;
-		protected static IntPtr detourPtr;
+		static IntPtr hWnd;
+		static IntPtr oldWndProcPtr;
+		static IntPtr detourPtr;
 
-		public static void Initialize(IntPtr hWnd) {
-			WndProcHandler.hWnd = hWnd;
+		public static void Initialize(IntPtr newHWnd) {
+			hWnd = newHWnd;
 
 			wndProcDelegate = WndProcDetour;
 			detourPtr = Marshal.GetFunctionPointerForDelegate(wndProcDelegate);
-			oldWndProcPtr = NativeMethods.SetWindowLongPtr(hWnd, WindowLongType.GwlWndProc, detourPtr);
+			oldWndProcPtr = NativeMethods.SetWindowLongPtr(newHWnd, WindowLongType.GwlWndProc, detourPtr);
 		}
 
 		public static void Shutdown() {
 			// If the current pointer doesn't match our detour, something swapped the pointer out from under us -
 			// likely the InterfaceManager doing its own cleanup. Don't reset in that case, we'll trust the cleanup
 			// is accurate.
-			var curWndProcPtr = NativeMethods.GetWindowLongPtr(hWnd, WindowLongType.GwlWndProc);
-			if (oldWndProcPtr != IntPtr.Zero && curWndProcPtr == detourPtr) {
-				NativeMethods.SetWindowLongPtr(hWnd, WindowLongType.GwlWndProc, oldWndProcPtr);
-				oldWndProcPtr = IntPtr.Zero;
+			IntPtr curWndProcPtr = NativeMethods.GetWindowLongPtr(hWnd, WindowLongType.GwlWndProc);
+			if (oldWndProcPtr == IntPtr.Zero || curWndProcPtr != detourPtr) {
+				return;
 			}
+
+			NativeMethods.SetWindowLongPtr(hWnd, WindowLongType.GwlWndProc, oldWndProcPtr);
+			oldWndProcPtr = IntPtr.Zero;
 		}
 
-		protected static long WndProcDetour(IntPtr hWnd, uint msg, ulong wParam, long lParam) {
+		static long WndProcDetour(IntPtr nhWnd, uint msg, ulong wParam, long lParam) {
 			// Ignore things not targeting the current window handle
-			if (hWnd == WndProcHandler.hWnd) {
-				var resp = WndProcMessage?.Invoke((WindowsMessage)msg, wParam, lParam);
+			if (nhWnd == hWnd) {
+				(bool, long)? resp = WndProcMessage?.Invoke((WindowsMessage)msg, wParam, lParam);
 
 				// Item1 is a bool, where true == capture event. If false, we're falling through default handling.
 				if (resp.HasValue && resp.Value.Item1) {
@@ -45,7 +47,7 @@ namespace NextUIPlugin.Overlay {
 				}
 			}
 
-			return NativeMethods.CallWindowProc(oldWndProcPtr, hWnd, msg, wParam, lParam);
+			return NativeMethods.CallWindowProc(oldWndProcPtr, nhWnd, msg, wParam, lParam);
 		}
 	}
 }
