c44a1d01fb38960b624bbe6364e1fce28069f72f
Pawel <wilkowski@gmail.com>
2022-02-01 08:30:46 +0100

    first working version

--- NextUIPlugin/Cef/CefHandler.cs
+++ NextUIPlugin/Cef/CefHandler.cs
@@ -16,9 +16,6 @@ namespace NextUIPlugin.Cef {
 			string cefDir,
 			string pluginDir
 		) {
-			// PluginLog.Log(Path.Combine(cefDir, "CustomSubProcess.exe"));
-			// PluginLog.Log("exi" + File.Exists(Path.Combine(cefDir, "CustomSubProcess.exe")));
-			// PluginLog.Log("log " + Path.Combine(pluginDir, "cef-debug.log"));
 			var settings = new CefSettings() {
 				CachePath = cacheDir,
 				MultiThreadedMessageLoop = true,
@@ -26,31 +23,20 @@ namespace NextUIPlugin.Cef {
 				WindowlessRenderingEnabled = true,
 				// BrowserSubprocessPath = Path.Combine(cefDir, "CefSharp.BrowserSubprocess.exe"),
 				BrowserSubprocessPath = Path.Combine(cefDir, "CustomSubProcess.exe"),
-				// CefCommandLineArgs = {
-				// 	["autoplay-policy"] = "no-user-gesture-required",
-				// },
 #if DEBUG
 				LogFile = Path.Combine(pluginDir, "cef-debug.log"),
 #else
-				// Don't log useless stuff for release
 				LogSeverity = CefLogSeverity.Fatal,
 #endif
 			};
 
-			// settings.CefCommandLineArgs["allow-no-sandbox-job"] = "1";
-			// settings.CefCommandLineArgs["enable-begin-frame-scheduling"] = "1";
-			// settings.CefCommandLineArgs.Remove("enable-system-flash");
-
-			// settings.EnableAudio();
-			// settings.SetOffScreenRenderingBestPerformanceArgs();
-			
 			var mainArgs = new CefMainArgs(Array.Empty<string>());
 			var cefApp = new NUCefApp();
-			//CefRuntime.Load(cefDir);
+
+			CefRuntime.Load(cefDir);
 			CefRuntime.EnableHighDpiSupport();
 			CefRuntime.Initialize(mainArgs, settings, cefApp, IntPtr.Zero);
 
-			// CefSharp.Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
 			// Otherwise it's a rare chance that making new browser window hangs up
 			Thread.Sleep(100);
 #if DEBUG
@@ -70,7 +56,6 @@ namespace NextUIPlugin.Cef {
 			 * Nasty trash garbonzo, don't question, Cef.Shutdown does NOT work properly.
 			 * If you have better solution, hit me up
 			 */
-			// foreach (var process in Process.GetProcessesByName("CefSharp.BrowserSubprocess")) {
 			foreach (var process in Process.GetProcessesByName("CustomSubProcess")) {
 				PluginLog.Log("KILLED CEF " + process.Id);
 				process.Kill();
@@ -80,7 +65,6 @@ namespace NextUIPlugin.Cef {
 			//CefRuntime.Shutdown();
 #else
 			// However in prod we gotta shutdown in order to save user data to cache.
-			//CefSharp.Cef.Shutdown();
 			CefRuntime.Shutdown();
 #endif
 		}
